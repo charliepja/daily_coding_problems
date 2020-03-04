fe54b9e4e27c35ac54c673f149bf97c4091642f6
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-04 18:31:00 +0000

    passes tests 1 & 2

--- code_problems/day1.js
+++ code_problems/day1.js
@@ -1,6 +1,8 @@
 // Given a list of numbers and a number k
 // Return whether any two numbers from the list
 // add up to k
-module.exports.numbersInArrayEqualK = async (numbers, k) => {
-
+module.exports.numbersInArrayEqualK = async (listOfNumbers, valueOfk) => {
+	if (listOfNumbers.length === 0 || !Number.isInteger(parseInt(valueOfk))) return false;
+	const numbers = listOfNumbers;
+	const k = valueOfk;
 };
--- test/test.js
+++ test/test.js
@@ -4,8 +4,19 @@ const day1 = require(path.resolve('code_problems', 'day1.js'));
 
 describe('Daily Coding Problems', function() {
 	describe('Day 1', function() {
-		it('should return true if two numbers within an array add up to the value of k', function() {
-			assert.equal(day1.numbersInArrayEqualK([10, 15, 3, 7], 17), true)
+		it('should return false if listOfNumbers is empty', async function() {
+			const result = await day1.numbersInArrayEqualK([], 17);
+			assert.equal(result, false)
+		});
+
+		it('should return false if k is not a number', async function() {
+			const result = await day1.numbersInArrayEqualK([10, 15, 3, 7], 'a');
+			assert.equal(result, false)
+		});
+
+		it('should return true if two numbers within an array add up to the value of k', async function() {
+			const result = await day1.numbersInArrayEqualK([10, 15, 3, 7], 17);
+			assert.equal(result, true)
 		});
 	});
 });
