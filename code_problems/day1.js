1c2192febef3f23672d6960420e985ed235d3ac1
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-04 19:04:01 +0000

    passes initial tests

--- code_problems/day1.js
+++ code_problems/day1.js
@@ -2,7 +2,17 @@
 // Return whether any two numbers from the list
 // add up to k
 module.exports.numbersInArrayEqualK = async (listOfNumbers, valueOfk) => {
-	if (listOfNumbers.length === 0 || !Number.isInteger(parseInt(valueOfk))) return false;
-	const numbers = listOfNumbers;
-	const k = valueOfk;
+	if (listOfNumbers.length < 2 || !Number.isInteger(parseInt(valueOfk))) return false;
+	const regex = /([A-Za-z])/g;
+	const numbers = [];
+	listOfNumbers.forEach(number => {
+		const stringNumber = number.toString();
+		const isMatch = stringNumber.match(regex);
+		if(!isMatch) {
+			numbers.push(parseInt(number));
+		}
+	});
+	if (numbers.length < 2) return false;
+	const totalOfNumbers = numbers.reduce((acc, cur) => acc + cur);
+	if (totalOfNumbers < valueOfk) return false;
 };
--- test/test.js
+++ test/test.js
@@ -14,6 +14,21 @@ describe('Daily Coding Problems', function() {
 			assert.equal(result, false)
 		});
 
+		it('should return false if listOfNumbers contains only 1 number', async function() {
+			const result = await day1.numbersInArrayEqualK([10], 17);
+			assert.equal(result, false)
+		});
+
+		it('should return false if the total of all numbers is less than k', async function() {
+			const result = await day1.numbersInArrayEqualK([4, 1, 3], 17);
+			assert.equal(result, false)
+		});
+
+		it('should return false if array is just alphabetic characters', async function() {
+			const result = await day1.numbersInArrayEqualK(['four', 'five', 'seven', 'ten'], 17);
+			assert.equal(result, false)
+		});
+
 		it('should return true if two numbers within an array add up to the value of k', async function() {
 			const result = await day1.numbersInArrayEqualK([10, 15, 3, 7], 17);
 			assert.equal(result, true)
