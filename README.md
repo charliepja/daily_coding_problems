88e0fa292eb2eb3a4c8a739854d4760c42295c78
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-04 19:31:27 +0000

    Updates readme and adds comments to code

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# daily_coding_problems
- 
+# Daily Coding Problems: JavaScript Edition
+### Challenges provided by: https://www.dailycodingproblem.com/
+
+Daily coding problems provides daily challenges that are similar to those that a developer would encounter during a technical test.
+The aim of the website is to help developers refine their interview skills, and they source their challenges from developers who have worked
+at top-tier companies.
+
+---
+
+## Day 1
+
+*Given a list of numbers and a number k, return whether any two numbers from the list add up to k*
+
+```js
+numbers = [10, 15, 3, 7]
+k = 17
+
+Since 10 + 7 = 17, should return true
+```
+
+### Initial Planning
+
+```
+if numbers total < k return false
+
+for number of numbers
+	doesEqual = false
+	i = 1
+
+	while doesEqual = false
+		if number > k return doesEqual = false
+		if number + number[i] != k
+			i++
+			return doesEqual = false
+
+	return doesEqual
+```
--- code_problems/day1.js
+++ code_problems/day1.js
@@ -1,10 +1,19 @@
 // Given a list of numbers and a number k
 // Return whether any two numbers from the list
 // add up to k
+
+
 module.exports.numbersInArrayEqualK = async (listOfNumbers, valueOfk) => {
+	// Checks that listOfNumbers contains at least 2 items, and that valueOfk is a number
 	if (listOfNumbers.length < 2 || !Number.isInteger(parseInt(valueOfk))) return false;
+
+	// regex to filter out any alphabetic characters or words
 	const regex = /([A-Za-z])/g;
+
+	// empty array to take in any non-alphabetic characters or words
 	const numbers = [];
+
+	// loop to filter against the regex and uses forEach loop due to not needing to return out of the loop
 	listOfNumbers.forEach(number => {
 		const stringNumber = number.toString();
 		const isMatch = stringNumber.match(regex);
@@ -12,13 +21,20 @@ module.exports.numbersInArrayEqualK = async (listOfNumbers, valueOfk) => {
 			numbers.push(parseInt(number));
 		}
 	});
+
+	// Checks that new array has at least 2 items
 	if (numbers.length < 2) return false;
+
+	// Gets the total of the new array and checks that the total is greater than valueOfk
+	// Does this check to ensure that it is possible that 2 numbers could equal valueOfk
 	const totalOfNumbers = numbers.reduce((acc, cur) => acc + cur);
 	if (totalOfNumbers < valueOfk) return false;
 
+	// declares conditions of while loop
 	let doesEqualK = false;
 	let i = 1;
 
+	// uses while loop as the loop should only continue while certain conditions are met
 	while((doesEqualK === false && i < numbers.length)) {
 		const equation = numbers[0] + numbers[i];
 		if(equation === valueOfk) return doesEqualK = true;
