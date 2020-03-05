7b392ea5da987a071fface5eac778f56aafdd4b7
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-05 21:15:19 +0000

    passes day2 test 3

--- code_problems/day2.js
+++ code_problems/day2.js
@@ -4,5 +4,5 @@ const functions = require(path.resolve('code_problems', 'helpers.js'));
 module.exports.productOfNumbers = async (numbers) => {
 	const newArray = await functions.removeStringsFromArray(numbers);
 	if(numbers.length === 0 || newArray.length === 0) return 'No numbers';
-
+	if(newArray.length < 3) return 'Not enough numbers';
 };
