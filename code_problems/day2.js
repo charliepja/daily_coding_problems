ee242b89e7ede23c14ee8cb60eebe872ac017b68
Charlie <19439935+charliepja@users.noreply.github.com>
2020-03-05 21:34:19 +0000

    Adds comments to code for day2

--- code_problems/day2.js
+++ code_problems/day2.js
@@ -1,13 +1,26 @@
+// Given an array of integers,
+// return a new array such that each element at index i of the new array
+// is the product of all the numbers in the original array except the one at i.
+
 const path = require('path');
 const functions = require(path.resolve('code_problems', 'helpers.js'));
 
 module.exports.productOfNumbers = async (numbers) => {
+	// Helper function which removes and non-number string from the array
+	// and converts number strings to ints
 	const newArray = await functions.removeStringsFromArray(numbers);
+
+	// Checks that it is possible to return an array that is product of all numbers
 	if(numbers.length === 0 || newArray.length === 0) return 'No numbers';
 	if(newArray.length < 3) return 'Not enough numbers';
 
+	// Initialises a new array to take in the result of each iteration of the for loop
 	const resultArray = [];
 
+	// For loop which creates a shallow copy of the newArray array
+	// removes the element at index i
+	// Gets the product of the remaining numbers
+	// Adds the result to the resultArray
 	for(const i in newArray) {
 		const cloneArray = Array.from(newArray);
 		cloneArray.splice(i, 1);
@@ -15,5 +28,6 @@ module.exports.productOfNumbers = async (numbers) => {
 		resultArray.push(result);
 	}
 
+	// Returns the result of the for loop
 	return resultArray;
 };
